a517ae26e2d1f6ef39ea298f5c551e7509449bca
Kevin Bradwick <kbradwick@gmail.com>
2011-07-30 00:22:24 +0100

    updated autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,7 @@ spl_autoload_register(function($className){
     $parts = explode('\\', $className);
     
     if ($parts[0] === 'Wildkat') {
-        $path = str_replace('\\', '/', $className) . '.php';
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
         $file = sprintf('%s/%s', SRC_DIR, $path);
         if (file_exists($file) === true) {
             require_once $file;
